24ec735548fa69efa40a6dfba832dd004dd6324f
Samuel Trahan <samtrahan91@gmail.com>
2020-11-12 20:02:46 -0600

    nav and basics setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
 
 export default function App() {
+  const [term, setTerm] = useState("");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {}, []);
+
+  const handleSubmit = (e, item) => {
+    console.log(e.target.value);
+    e.preventDefault();
+    setItems(e.target.value);
+  };
+
+  const onInputChange = (e) => {
+    setTerm(e.target.value);
+  };
+
+  const renderItems = () => {
+    return items.map((item) => <div>{item}</div>);
+  };
+
   return (
     <div>
-      Hello World
+      <NavBar />
+      <div>{renderItems()}</div>
+      <form onSubmit={handleSubmit}>
+        <div className="input">
+          <input
+            onChange={onInputChange}
+            type="text"
+            className="input-box"
+            placeholder="Add items..."
+          ></input>
+          <button className="button">Add</button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NavBar.css';
+
+export default function NavBar() {
+  return (
+    <nav className="nav">
+      <img className="logo" alt="" src="/images/bryd.png"></img>
+    </nav>
+  )
+}
