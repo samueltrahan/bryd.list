2aa24477d399931c085214b6e9d6500d10d7adb6
Samuel Trahan <samtrahan91@gmail.com>
2020-11-19 12:44:25 -0600

    service worker is working

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -17,10 +17,27 @@ self.addEventListener('install', (event) => {
 
 //Listen to requests
 self.addEventListener('fetch', (event) => {
-
+    event.respondWith(
+      caches.match(event.request)
+      .then(() => {
+        return fetch(event.request)
+          .catch(() => caches.match('offline.html'))
+      })
+    )
 });
 
-//Activate teh Service Worker
+//Activate the Service Worker
 self.addEventListener('activate', (event) => {
-
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
+  event.waitUntil(
+    caches.keys()
+    .then((cacheNames) => Promise.all(
+      cacheNames.map((cacheName) => {
+        if(!cacheWhiteList.includes(cacheName)) {
+          return caches.delete(cacheName);
+        }
+      })
+    ))
+  )
 });
