bca6a92bec2c75895eaa22e4a8efc7a914e5378d
Samuel Trahan <samtrahan91@gmail.com>
2020-11-19 13:38:14 -0600

    nav bar color change'

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -15,10 +15,40 @@ self.addEventListener("install", (event) => {
 });
 
 //Listen to requests
-self.addEventListener("fetch", (event) => {
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then(() => {
+//       return fetch(event.request).catch(() => caches.match("offline.html"));
+//     })
+//   );
+// });
+
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match("offline.html"));
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then(function (response) {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        // IMPORTANT: Clone the response. A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        var responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
     })
   );
 });
